8dc025b4a20aedd9fd18e9354b9d30bb3a5e61e1
Yannick Saive <saive@kth.se>
2019-07-24 20:15:05 +0200

    same

--- mdhtml/convert.go
+++ mdhtml/convert.go
@@ -1,8 +1,31 @@
 package mdhtml
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func Convert(){
-	fmt.Println("Found it")
-	return
+
+// md_bytes should be populated with the .md file. This function is the wrapper
+// function, which calls all other functions. It returns a []byte with the .md
+// file converted to .html syntax.
+func Convert(md_bytes []byte) []byte{
+	fmt.Println(string(md_bytes))
+	return md_bytes
 }
+
+
+// splits the bytes into an array of lines
+func toLines(b []byte) []string{
+	stringArr := strings.Split(string(b), "\n")
+	fmt.Println(stringArr)
+
+	var nonNil []string
+	for _, str := range stringArr {
+		if str != "" {
+			nonNil = append(nonNil, str)
+		}
+	}
+	return nonNil 
+}
+
