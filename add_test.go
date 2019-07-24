8e60801172f270ecbf8f68049fd7dba72660f508
Yannick Saive <saive@kth.se>
2019-07-24 16:45:15 +0200

    travis

--- add_test.go
+++ add_test.go
@@ -2,15 +2,15 @@ package mdhtml
 
 import "testing"
 
-type testpair struct {
-	values []int
-	sum int 
-  }
-var tests = []testpair{
-	{[]int{1,2}, 3},
-	{[]int{2,-1}, 1},
-}
 func TestAdd(t *testing.T){
+	type testpair struct {
+		values []int
+		sum int 
+	  }
+	var tests = []testpair{
+		{[]int{1,2}, 3},
+		{[]int{2,-1}, 1},
+	}
 	for _, pair := range tests {
 		v := Add(pair.values[0], pair.values[1])
 		if v != pair.sum {
