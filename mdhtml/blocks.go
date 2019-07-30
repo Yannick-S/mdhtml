81a97d4e26c5bdf5e50768a163f6ab2a53b75209
Yannick S <saive@kth.se>
2019-07-30 18:37:49 +0200

    work on blocks

--- mdhtml/blocks.go
+++ mdhtml/blocks.go
@@ -4,24 +4,41 @@ import (
 	"errors"
 	"strings"
 )
+
 //This function returns a string, which indicates what type of a block this is
 // This works well, since there are only a few types of blocks.
 
 func determineBlock(block []string) (string, error) {
+	//empty stuff
 	if block == nil {
 		return "", errors.New("No block type")
 	}
-	if len(block) <= 1 {
+	if len(block) < 1 {
 		return "", errors.New("No block type")
 	}
 	//White Space stuff first
 	trim := strings.TrimSpace(block[0])
 	if trim == "" {
-		return "" , errors.New("First line empty")
+		return "", errors.New("First line empty")
 	}
-	if block[0][0] == '>' {
-		return "Blockquotes", nil
+	if len(block[0]) > 0 && block[0][0:1] == " " {
+		return "", errors.New("First is space")
 	}
 
+	if block[0][0] == '>' {
+		if block[0][1] == ' ' {
+			return "Blockquotes", nil
+		}
+	}
+	if block[0][0] == '\t' {
+		if block[0][1] == ' ' {
+			return "Code", nil
+		}
+	}
+	if block[0][0] == '*' || block[0][0] == '+' || block[0][0] == '-' {
+		if block[0][1] == ' ' {
+			return "List", nil
+		}
+	}
 	return "", errors.New("No block type identified")
 }
--- mdhtml/blocks_test.go
+++ mdhtml/blocks_test.go
@@ -1,33 +1,38 @@
 package mdhtml
 
 import (
-	"testing"
 	"errors"
+	"testing"
 )
 
-func TestDetermineBlock(t *testing.T){
+func TestDetermineBlock(t *testing.T) {
 	type tests struct {
 		Input  []string
 		Output string
-		Error error
+		Error  error
 	}
 	var testPairs = []tests{
-		{nil,"", errors.New("No block type")},
-		{[]string{},"", errors.New("No block type")},
-		{[]string{"","abc","\tabcd"},"", errors.New("First line empty")},
-		{[]string{" ","abc","\tabcd"},"", errors.New("First line empty")},
-		{[]string{"\t","abc","\tabcd"},"", errors.New("First line empty")},
-		{[]string{"\n","abc","\tabcd"},"", errors.New("First line empty")},
-		{[]string{" \t  \n \t","abc","\tabcd"},"", errors.New("First line empty")},
+		{nil, "", errors.New("No block type")},
+		{[]string{}, "", errors.New("No block type")},
+		{[]string{"", "abc", "\tabcd"}, "", errors.New("First line empty")},
+		{[]string{" ", "abc", "\tabcd"}, "", errors.New("First line empty")},
+		{[]string{"\t", "abc", "\tabcd"}, "", errors.New("First line empty")},
+		{[]string{"\n", "abc", "\tabcd"}, "", errors.New("First line empty")},
+		{[]string{" \t  \n \t", "abc", "\tabcd"}, "", errors.New("First line empty")},
+		{[]string{" \tbla  \n \t", "abc", "\tabcd"}, "", errors.New("First is space")},
+		{[]string{">bla  \n \t", "  abc", "  \tabcd"}, "Blockquotes", nil},
+		{[]string{">bla  \n \t", "  abc", "  \tabcd"}, "Blockquotes", nil},
 		//{[]string{"abc","def","ghi"},	"paragraph", nil},
 	}
 	for _, pair := range testPairs {
 		result, err := determineBlock(pair.Input)
-		if err.Error() != pair.Error.Error() {
-			t.Error("Error Finding Block Type:", pair.Input,
-				"expected error", pair.Error,
-				"got", err,
-			)
+		if err != nil {
+			if err.Error() != pair.Error.Error() {
+				t.Error("Error Finding Block Type:", pair.Input,
+					"expected error", pair.Error,
+					"got", err,
+				)
+			}
 		}
 		if result != pair.Output {
 			t.Error("Error Finding Block Type:", pair.Input,
